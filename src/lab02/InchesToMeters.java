9e4130e77e415a43b66748955604c60638f1a552
Christopher201 <Christopher.Fioti1@marist.edu>
2020-01-30 19:26:32 -0500

    Updated conversion and imports, added loads of comments

--- src/lab02/InchesToMeters.java
+++ src/lab02/InchesToMeters.java
@@ -1,5 +1,5 @@
 package lab02;
-import java.util.*;
+import java.util.Scanner;
 
 public class InchesToMeters {
 
--- src/lab02/TemperatureConverter.java
+++ src/lab02/TemperatureConverter.java
@@ -1,42 +1,55 @@
 package lab02;
 
-import java.util.*;
+import java.util.Scanner;
 
-public class TemperatureConverter
-{
+// Creates the Public class
+public class TemperatureConverter {
 
+    // Allows for keyboard input
     private static Scanner keyboard = new Scanner(System.in);
 
-    public static void main(String[] args)
-    {
+    // Main function
+    public static void main(String[] args) {
+
+        // Opening Statement
         System.out.println("Hello and welcome to the temperature converter!");
         System.out.println("Please, input the current degrees and what scale it is in using C or F");
+
+        // Gets user input
         String entry = keyboard.nextLine();
 
+            // Takes the string, parses it for ints, and creates "format" and "degrees"
             String value = entry.substring(0, entry.length() - 1);
             double degrees = Double.parseDouble(value);
             char format;
             format = entry.charAt(entry.length() - 1);
 
-        if(format == 'C' || format =='c')
-        {
+        // Checks for celsius format
+        if(format == 'C' || format =='c') {
+
+            // Converts to Fahrenheit
             double answer = ((9 / 5.0)*degrees) + 32;
             System.out.println("You put in " + degrees + " C and we calculated this is equal to " + answer +
                     " degrees fahrenheit!");
-        }
-        if(format == 'F' || format == 'f')
-        {
+        } //If C
+
+        // Checks for Fahrenheit format
+        if(format == 'F' || format == 'f') {
+
+            // Converts to celsius
             double answer = (5/9.0)*(degrees - 32);
             System.out.println("You put in " + degrees + " F and we calculated this is equal to " + answer +
                     " degrees celsius!");
 
-        }
-        else
-        {
+        } // if F
+
+        // Error return
+        else {
             System.out.println("Please enter a C or an F at the end of your number!");
-        }
+        } // Else
+        // End statement
         System.out.println("Thank you for using this service! Have a wonderful day!");
 
 
-    }
-}
+    } // Main
+} // TemperatureConverter
