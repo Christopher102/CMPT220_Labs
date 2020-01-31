f2b06944c26f51f0f24e1987aa88111b20f644af
Christopher201 <Christopher.Fioti1@marist.edu>
2020-01-30 19:30:03 -0500

    Updated conversion and imports, added loads of comments, also changed my conversion factor to final

--- src/lab02/InchesToMeters.java
+++ src/lab02/InchesToMeters.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 public class InchesToMeters {
 
     static Scanner keyboard = new Scanner(System.in);
-    private static double convert;
 
     public static void main(String[] args) {
 
-        convert = .0254;
+        final double convert = .0254;
 
         System.out.println("Hello! This program converts inches to meters!");
         System.out.println("Please enter in the number of inches you wish to convert!");
